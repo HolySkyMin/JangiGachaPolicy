ee6c23842a25c5121af953b4cb0e2ff5fa1e31b2
EQwon <bluetwintail@naver.com>
2018-07-07 16:45:03 +0900

    Pop Up Result When You Do Gacha

--- Assets/Scripts/ShowGachaResult.cs
+++ Assets/Scripts/ShowGachaResult.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 
 public class ShowGachaResult : MonoBehaviour
 {
+	public GameObject ResultPanel;
 	public Text[] Amount;
+
+	private void Start()
+	{
+		ResultPanel.SetActive(false);
+	}
 
 	public void UpdateResult(int[] resultArray)
 	{
+		ResultPanel.SetActive(true);
+
 		for(int i = 0; i < resultArray.Length; i++)
 		{
 			Amount[i].text = resultArray[i].ToString();
