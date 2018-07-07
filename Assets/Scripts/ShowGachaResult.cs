7d18ab94642f58a9b3b7c866ab460fcfe4d76214
EQwon <bluetwintail@naver.com>
2018-07-07 15:51:22 +0900

    Show Gacha Result

---
+++ Assets/Scripts/ShowGachaResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowGachaResult : MonoBehaviour
+{
+	public Text[] Amount;
+
+	public void UpdateResult(int[] resultArray)
+	{
+		for(int i = 0; i < resultArray.Length; i++)
+		{
+			Amount[i].text = resultArray[i].ToString();
+		}
+	}
+}
