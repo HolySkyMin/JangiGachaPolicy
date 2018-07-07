b239c38c44ab776141e0de1a883b9838e6d654e2
thiEFcat <fsda109238@gmail.com>
2018-07-08 06:13:24 +0900

    Fourth Playtest Version

---
+++ Assets/Scripts/ScreenScaler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScaler : MonoBehaviour
+{
+	public CanvasScaler scaler;
+
+	private void Start()
+	{
+		SetScale(Screen.width, Screen.height);
+	}
+
+	public void SetScale(int width, int height)
+	{
+		if(height / (float)width >= 16f / 9f)
+			scaler.matchWidthOrHeight = 0f;
+		else
+			scaler.matchWidthOrHeight = 1f;
+	}
+}
